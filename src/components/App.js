99e0330dcd42d82c50b6b82e8d0f438ce11347de
rowaxl <rowaxl0@gmail.com>
2019-01-20 21:11:35 +0900

    show search result

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import ImageClient from './ImageClient';
+import Image from './Image';
 
 class App extends React.Component {
     state = { images: [] }
@@ -11,10 +12,20 @@ class App extends React.Component {
     }
 
     render() {
+        const imageList = this.state.images.map((image, i) => {
+            return <Image src={image.urls.regular} key={i} />
+        })
+        console.log(imageList)
+
         return (
-            <div className="ui container" style={{ marginTop: "10px" }}>
-                <SearchBar onSubmit={this.onSearchSubmit} />
-                Found { this.state.images.length } images
+            <div>
+                <div className="ui container" style={{ marginTop: "10px" }}>
+                    <SearchBar onSubmit={this.onSearchSubmit} />
+                    Found {this.state.images.length} images
+                </div>
+                <div className="ui container" style={{ marginTop: "10px", display: "grid" }}>
+                    {imageList}
+                </div>
             </div>
         )
     }
--- src/components/ImageList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-class ImageList extends React.Component {
-
-}
-
-export default ImageList;
